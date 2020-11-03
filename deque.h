0e250fb4eea0d7d06266da319182b81a3d4203d6
gjnmbm <gjnmbm@gmail.com>
2020-11-03 10:15:11 -0800

    Add to deque.h and test app

--- deque.h
+++ deque.h
@@ -1,15 +1,18 @@
-#ifndef
-#define
+#ifndef DEQUE_H
+#define DEQUE_H
 struct deque;
 struct link;
 
-void deque_insert_front(struct deque*, void* item);
-void deque_insert_last(struct deque*, void* item);
+struct deque* deque_create();
+void deque_free(struct deque* dq);
+void deque_insert_front(struct deque* dq, void* item);
+void deque_insert_last(struct deque* dq, void* item);
 void deque_delete_front(struct deque* dq);
 void deque_delete_last(struct deque* dq);
 void* deque_get_front(struct deque* dq);
 void* deque_get_rear(struct deque* dq);
 int deque_is_empty(struct deque* dq);
 //Can add is_full method, but might be useless if it can resize itself
+//A person could define what it means for a deque to be full though
 
 #endif
