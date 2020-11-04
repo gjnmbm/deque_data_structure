ea99a896dd1143973132937cdba72836fb2d8143
gjnmbm <gjnmbm@gmail.com>
2020-11-03 19:23:16 -0800

    Change function names and tests

--- deque.h
+++ deque.h
@@ -5,12 +5,12 @@ struct link;
 
 struct deque* deque_create();
 void deque_free(struct deque* dq);
-void deque_insert_front(struct deque* dq, void* item);
-void deque_insert_last(struct deque* dq, void* item);
-void deque_delete_front(struct deque* dq);
-void deque_delete_last(struct deque* dq);
-void* deque_get_front(struct deque* dq);
-void* deque_get_rear(struct deque* dq);
+void deque_push_front(struct deque* dq, void* item);
+void deque_push_last(struct deque* dq, void* item);
+void deque_pop_front(struct deque* dq);
+void last(struct deque* dq);
+void* deque_peek_front(struct deque* dq);
+void* deque_peek_rear(struct deque* dq);
 int deque_is_empty(struct deque* dq);
 //Can add is_full method, but might be useless if it can resize itself
 //A person could define what it means for a deque to be full though
--- test_deque.c
+++ test_deque.c
@@ -1,1 +1,26 @@
+/*
+ * This file contains executable code to test the functionality
+ * of the deque. 
+ *
+ * By Kristina Mason
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "deque.h"
+
+int main(int argc, char** argv) {
+	struct deque* dq;
+	int test_arr[10];
+	for (int i = 0; i < 10; i++) {
+		test_arr[i] = i;
+	}
+	printf("Pushing 5 elements to front of queue...\n");
+	printf("Pushing 5 elements to back of queue...\n");
+	printf("Popping first 3 elements from front...\n");
+	printf("Popping last 3 elements from back...\n");
+	printf("Popping the rest of the elements...\n");
+	printf("Is deque empty? (Expecting 1)", deque_is_empty(dq));
+	return 0;
+}
