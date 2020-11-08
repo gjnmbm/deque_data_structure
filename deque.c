2d091d1d79452f2dac508a0a70ede8549c7f8759
gjnmbm <gjnmbm@gmail.com>
2020-11-07 20:42:42 -0800

    Add more methods

--- deque.c
+++ deque.c
@@ -12,3 +12,18 @@ struct link {
 	struct link* next;
 	void* val;
 };
+
+struct deque* deque_create() {
+	struct deque* dq = malloc(sizeof(struct deque));
+	dq->front_link = NULL;
+	dq->back_link = NULL;
+	return dq;
+}
+
+void deque_free(struct deque* dq) {
+	free(dq);
+}
+
+void deque_push_front(struct deque* dq, void* item) {
+	
+}
